d2130f84910d751c807e0e234ce21238fcf6393e
Veverka Martin <Martin.Veverka@turboconsult.cz>
2020-01-14 16:16:13 +0100

    Program pro generování GPX zatím prázdný.

--- src/Konst.hs
+++ src/Konst.hs
@@ -1,6 +1,6 @@
 module Konst
     (
-      dirRoot, dir1srtm, dir2srtm, dir3vrcholy
+      dirRoot, dir1srtm, dir2srtm, dir3vrcholy, dir4gpx
     ) where
 
 import System.FilePath.Posix
@@ -9,4 +9,5 @@ dirRoot = "g:/vrch"
 
 dir1srtm = dirRoot </> "1srtm"
 dir2srtm = dirRoot </> "2srtm"
-dir3vrcholy = dirRoot </> "vrcholy"
+dir3vrcholy = dirRoot </> "3vrch"
+dir4gpx = dirRoot </> "4gpx"
---
+++ vrcholy-gpx/MainGpx.hs
@@ -0,0 +1,40 @@
+module MainGpx where
+
+import Lib
+import Nacitac
+import Zapisovac
+import Hledac
+import Potopa
+import Uzemi
+import VrchTypy
+import PripravaVstupu
+
+import System.FilePath.Posix
+import Konst
+import System.Directory
+import qualified Data.ByteString as B
+import qualified Data.Map.Lazy as M
+import  Control.Arrow
+import  Control.Monad
+
+
+
+writeVysledekV :: String -> [Vrch] -> IO()
+writeVysledekV filename body = do
+    let fullFn =  dir4gpx </> filename
+--    toto čerpá strašně moc ramky    
+--    putStrLn $ "Zapis "  ++ (show . length) body ++ " bodu do \"" ++ fullFn ++ "\""
+    writeFile fullFn (bodyXml body)
+
+main :: IO ()
+main = do
+    soubory <- listDirectory dir3vrcholy
+    putStrLn $ "Pocet souboru:     " ++  (show . length) soubory
+    forM_ soubory $ \fileName -> do
+        putStrLn $ "Prevod do GPX: " ++  (dir3vrcholy </> fileName) 
+        text <- readFile (dir3vrcholy </> fileName) 
+        let vrchy = map read (lines text) :: [Vrch]
+        putStrLn $ "Pocet vrchu:     " ++  (show . length) vrchy
+        createDirectoryIfMissing True dir4gpx
+        writeVysledekV (fileName ++ ".gpx") vrchy
+    
