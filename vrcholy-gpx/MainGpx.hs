a456de09716a7248fc3b5fc789dc86fd4547355a
Veverka Martin <Martin.Veverka@turboconsult.cz>
2020-01-20 14:22:22 +0100

    geonames vyzkoušet

--- vrcholy-gpx/MainGpx.hs
+++ vrcholy-gpx/MainGpx.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Lib
@@ -8,16 +10,21 @@ import Potopa
 import Uzemi
 import VrchTypy
 import PripravaVstupu
-
-import System.FilePath.Posix
 import Konst
+
+import Data.Aeson.Lens (key, nth, _String)
+--import Data.Aeson.Types (unpack)
+import qualified Data.Text as T
+import System.FilePath.Posix
 import System.Directory
 import qualified Data.ByteString as B
+import qualified Data.ByteString.UTF8 as B8
 import qualified Data.Map.Lazy as M
-import  Control.Arrow
-import  Control.Monad
-import  GHC.IO.Encoding
-
+import Control.Arrow
+import Control.Monad
+import GHC.IO.Encoding
+import Network.Wreq
+import Control.Lens
 
 writeVysledekV :: String -> [Vrch] -> IO()
 writeVysledekV filename body = do
@@ -38,4 +45,30 @@ main = do
         putStrLn $ "Pocet vrchu:     " ++  (show . length) vrchy
         createDirectoryIfMissing True dir4gpx
         writeVysledekV (fileName ++ ".gpx") vrchy
+    
+
+p :: IO ()
+p = do
+    let lat = 49.2854672 :: Double
+    let lng = 16.4719033 :: Double
+
+    let opts = defaults & param "lat" .~ [T.pack (show lat)] & param "lng" .~ [T.pack (show lng)] 
+    r <- getWith opts "http://api.geonames.org/findNearbyJSON?username=marvertin&verbosity=FULL&maxRows=100&radius=1"
+    -- print r
+    let statuskod = r ^. responseStatus . statusCode
+    print statuskod
+    --let pole = r ^? responseBody . key "geonames" . traverseArray 
+    -- let ss = unpack pole
+    -- print pole
+--    let body = r ^. responseBody
+--    putStrLn $ B8.toString body
+    let tono0 = r ^. responseBody . key "geonames" . nth 0 . key "toponymName" . _String
+    print tono0
+    let tono1 = r ^. responseBody . key "geonames" . nth 1 . key "toponymName" . _String
+    print tono1
+    let tono2 = r ^. responseBody . key "geonames" . nth 2 . key "toponymName" . _String
+    print tono2
+    let tono3 = r ^. responseBody . key "geonames" . nth 2 . key "toponymName" . _String
+    print tono3
+
     
