6fe919cb3642e08642db63b949b3828c86662e45
Veverka Martin <Martin.Veverka@turboconsult.cz>
2020-01-20 18:05:31 +0100

    Libs

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,10 @@
 module Lib
     (
-    grupuj, rozbal2, fst3, snd3, thr3, kazdyPrvekAZbytky, rostouci
+    grupuj, rozbal2, fst3, snd3, thr3, kazdyPrvekAZbytky, rostouci, base34
     ) where
 
 import qualified Data.Map.Lazy as M
+import Data.Char (chr)
 
 -- Zgrupuje seunam do mapy tak, že aplikue funkce na získání klíčů a hodnot a vrací 
 -- mapu klíčů na seznamy hodnot (takže v hodnotýách budou vždy neprázdné seznamy)             
@@ -37,3 +38,52 @@ rostouci :: (Enum a, Ord a) => [a] -> [a]
 rostouci [] = []
 rostouci [a] = [a]
 rostouci (a : b : rest) = (a : rostouci (max b (succ a) : rest))
+
+
+
+
+
+
+base34 ::  Integer -> String
+base34 0 = ""
+base34 n | n < 0 = error $ "zaporne cislo do base34: " ++ show n
+base34 n = let
+    zbytek = n `mod` 34
+    podil =  n `div` 34
+   in base34(podil) ++ [cnvt zbytek]
+
+
+cnvt 0 = '0'
+cnvt 1 = '1'
+cnvt 2 = '2'
+cnvt 3 = '3'
+cnvt 4 = '4'
+cnvt 5 = '5'
+cnvt 6 = '6'
+cnvt 7 = '7'
+cnvt 8 = '8'
+cnvt 9 = '9'
+cnvt 10 = 'A'
+cnvt 11 = 'B'
+cnvt 12 = 'C'
+cnvt 13 = 'D'
+cnvt 14 = 'E'
+cnvt 15 = 'F'
+cnvt 16 = 'G'
+cnvt 17 = 'H'
+cnvt 18 = 'J'
+cnvt 19 = 'K'
+cnvt 20 = 'L'
+cnvt 21 = 'M'
+cnvt 22 = 'N'
+cnvt 23 = 'P'
+cnvt 24 = 'Q'
+cnvt 25 = 'R'
+cnvt 26 = 'S'
+cnvt 27 = 'T'
+cnvt 28 = 'U'
+cnvt 29 = 'V'
+cnvt 30 = 'W'
+cnvt 31 = 'X'
+cnvt 32 = 'Y'
+cnvt 33 = 'Z'
